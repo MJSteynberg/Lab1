35c287ec57d9e791e541ab379b487e2c41680593
MJSteynberg <120567081+MJSteynberg@users.noreply.github.com>
2024-05-21 11:01:42 +0200

    Constructed Mglob.m

--- createMglob.m
+++ createMglob.m
@@ -1,14 +1,29 @@
 function [Mglob] = createMglob (Mloc, d_h)
-% function [Mglob] = createMglob (Mloc, d_h)
-% Mloc   :    local stiffness matrix on element of length h
-% d_h    :    number of intervals in partition Delta_h
-% Mglob  :    global stiffness matrix (after application of b.c.)
+    % function [Mglob] = createMglob (Mloc, d_h)
+    % Mloc   :    local stiffness matrix on element of length h
+    % d_h    :    number of intervals in partition Delta_h
+    % Mglob  :    global stiffness matrix (after application of b.c.)
+        
+    % Initialize
+    Mglob = sparse(2*(d_h+1), 2*(d_h+1));
     
-% Initialize
-Mglob = sparse(2*(d_h+1), 2*(d_h+1));
+    % Assemble
+    for l=1:d_h
+        Mlocfull = spalloc(2*d_h+2, 2*d_h+2,4*4);
+        for x=1:4
+            for y=1:4
+                Mlocfull(2*(l-1)+x, 2*(l-1) + y) = Mloc(x,y);
+            end 
+        end 
+        Mglob = Mglob + Mlocfull;
+    end
 
-% Assemble
+    dim = 2*d_h;
+    
+    I = eye(dim);
+    P = zeros(dim, dim+2);
+    P(:,2:dim+1) = I;
 
-
-
+    Mglob = P*Mglob*P';
 end
+
